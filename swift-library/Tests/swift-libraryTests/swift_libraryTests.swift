9e925f9e06f8e802aee96d43f8e4a1fc4f40ad71
Ruslan Horbachevskyi <ruslan.horbachevskyi@onix-systems.com>
2025-06-23 09:33:31 +0300

    Removed tests

--- swift-library/Tests/swift-libraryTests/swift_libraryTests.swift
+++
@@ -1,8 +0,0 @@
-import Testing
-import Foundation
-import class Foundation.Bundle
-@testable import swift_library
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
