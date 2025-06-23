89aca25f9ec0be10ca38a27c19d226b38a86936d
Ruslan Horbachevskyi <ruslan.horbachevskyi@onix-systems.com>
2025-06-23 09:44:17 +0300

    Removed tests

--- swift-library/Package.swift
+++ swift-library/Package.swift
@@ -6,19 +6,14 @@ import PackageDescription
 let package = Package(
     name: "swift-library",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "swift-library",
-            targets: ["swift-library"]),
+            targets: ["swift-library"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-library"),
-        .testTarget(
-            name: "swift-libraryTests",
-            dependencies: ["swift-library"]
-        ),
+            name: "swift-library"
+        )
     ]
 )
